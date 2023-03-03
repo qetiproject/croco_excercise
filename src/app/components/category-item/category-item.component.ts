13300bdbe19078a111efd3a85e7b3d68a15a8456
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-03-03 18:05:05 +0400

    design

---
+++ src/app/components/category-item/category-item.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-item',
+  templateUrl: './category-item.component.html',
+  styleUrls: ['./category-item.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategoryItemComponent {
+
+}
